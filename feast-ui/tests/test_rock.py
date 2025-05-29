b783a0794e53c0e2ce91c18a0c58420ecb10a33d
misohu <michal.hucko@canonical.com>
2025-05-29 18:39:31 +0200

    feat: Build the frontend to configure the PUBLIC_URL env var for exposure under a prefix
    Part of: canonical/feast-operators#23
    
    Rebuilds the feast-ui rock with custom built of feast ui for /feast ingress

--- feast-ui/tests/test_rock.py
+++ feast-ui/tests/test_rock.py
@@ -15,26 +15,7 @@ def test_rock():
     rock_version = check_rock.get_version()
     LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
 
-    # 1. Check that the feast package is installed
-    result = subprocess.run(
-        [
-            "docker",
-            "run",
-            "--entrypoint",
-            "/bin/bash",
-            LOCAL_ROCK_IMAGE,
-            "-c",
-            "pip list | grep '^feast '"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        check=True,
-    )
-
-    assert "feast" in result.stdout, "Feast is not installed"
-
-    # 2. Sanity check that the `feast ui --help` command runs successfully
+    # Sanity check that the `feast ui --help` command runs successfully
     result_ui = subprocess.run(
         [
             "docker",
@@ -52,3 +33,24 @@ def test_rock():
     )
 
     assert "Usage" in result_ui.stdout or "usage" in result_ui.stdout, "Feast UI CLI not functioning as expected"
+
+def test_feast_ui_index_has_correct_manifest_href():
+    """Ensure index.html contains href to /feast/manifest.json."""
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
+    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            "--entrypoint",
+            "/bin/bash",
+            LOCAL_ROCK_IMAGE,
+            "-c",
+            'grep -ri "href=\"/feast/manifest.json\"" /usr/local/lib/python3.10/dist-packages/feast/ui/build/index.html && exit 1 || exit 0',
+        ],
+        check=True,
+    )
