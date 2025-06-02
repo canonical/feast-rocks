09eb5240de545c7f5184fed46171af2e59118994
misohu <michal.hucko@canonical.com>
2025-06-02 12:59:40 +0200

    Build frontend with correct path prefix for /feast

--- feast-ui/tests/test_rock.py
+++ feast-ui/tests/test_rock.py
@@ -33,3 +33,24 @@ def test_rock():
     )
 
     assert "Usage" in result_ui.stdout or "usage" in result_ui.stdout, "Feast UI CLI not functioning as expected"
+
+def test_feast_ui_index_has_correct_manifest_href():
+    """Ensure index.html contains href to /feast/manifest.json."""
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
+    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            "--entrypoint",
+            "/bin/bash",
+            LOCAL_ROCK_IMAGE,
+            "-c",
+            'grep -ri "href=\"/feast/manifest.json\"" /usr/local/lib/python3.10/dist-packages/feast/ui/build/index.html && exit 1 || exit 0',
+        ],
+        check=True,
+    )
